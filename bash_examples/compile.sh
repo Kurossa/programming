47b5511cbd6e6f22bcf3b4be4bbeb8ab1d64010a
Kurossa <4mariusz4@wp.pl>
2017-03-19 12:47:37 +0100

    Change bash scripts to English. Add main function, clear console at begening of script

--- bash_examples/compile.sh
+++ bash_examples/compile.sh
@@ -1,50 +1,59 @@
 #!/bin/bash
-#Zadanie z maila
-#Kompilacja pliku example.cpp debug/release za pomocą g++
-# z użuciem dowolnej nazwy
+# Task from e-mail
+# Compile file example.cpp using g++ with debug/release flags
+# Use defuned file name as input parameter
+
+INPUT_FILE_TEXT="Enter output file name:"
+WARNING_TEXT="Such file name exists, enter new file name:"
+DONE_TEXT="Compilation Done"
+
 
 debug () {
-	echo "Podaj nazwę pliku wyjściowego:"
-	read nazwa
-		while [ -e "$nazwa" ] ; do
-		echo "Plik o takiej nazwie już istnieje. Podaj nową nazwę:"
-		read nazwa
+	echo $INPUT_FILE_TEXT
+	read file_name
+		while [ -e "$file_name" ] ; do
+		echo $WARNING_TEXT
+		read file_name
 		done
-	g++ -g -Wall -o $nazwa example.cpp
-	echo "Wykonano kompilację debug"
+	g++ -g -Wall -o $file_name example.cpp
+	echo $DONE_TEXT
 }
 
 release () {
-	echo "Podaj nazwę pliku wyjściowego:"
-	read nazwa
-		while [ -e "$nazwa" ] ; do
-		echo "Plik o takiej nazwie już istnieje. Podaj nową nazwę:"
-		read nazwa
+	echo $INPUT_FILE_TEXT
+	read file_name
+		while [ -e "$file_name" ] ; do
+		echo $WARNING_TEXT
+		read file_name
 		done
-	g++ -O2 -Wall -o $nazwa example.cpp
-	echo "Wykonano kompilację release"
+	g++ -O2 -Wall -o $file_name example.cpp
+	echo $DONE_TEXT
 }
 
-echo "Witaj"
-echo "Program kompiluje plik example.cpp"
-echo "1) Debug"
-echo "2) Release"
-echo "3) Wyjście"
-echo "Podaj rodzaj kompilacji:"
+main () {
+	clear
+	echo "Script compiles example.cpp file"
+	echo "1) Debug flag"
+	echo "2) Release flag"
+	echo "3) Exit script"
+	echo "What compilation flag you want to use:"
 
-read x
+	read x
 
+	if [ "$x" == "1" ] ; then
+		debug
 
+	elif [ "$x" == "2" ] ; then
+		release
 
-if [ "$x" == "1" ] ; then
-	debug
+	elif [ "$x" == "3" ]; then
+		echo "Exit, file not compiled."
+		exit
+	else 
+		echo "Not allowed switch!"
+		sleep 1
+		main
+	fi
+}
 
-elif [ "$x" == "2" ] ; then
-	release
-
-elif [ "$x" == "3" ]; then
-	echo "Do widzenia"
-	exit
-
-else echo "Nieznana kompilacja"
-fi
+main
