bbd875c5dfa6445856c2219075dac62428828446
Kurossa <4mariusz4@wp.pl>
2017-03-21 19:44:47 +0100

    Optimize example.sh script. Use only one function with different compilation parameters

--- bash_examples/compile.sh
+++ bash_examples/compile.sh
@@ -6,47 +6,44 @@
 INPUT_FILE_TEXT="Enter output file name:"
 WARNING_TEXT="Such file name exists, enter new file name:"
 DONE_TEXT="Compilation Done"
+COMPILE_FLAGS = ""
 
-
-debug () {
-	echo $INPUT_FILE_TEXT
-	read file_name
-		while [ -e "$file_name" ] ; do
-		echo $WARNING_TEXT
-		read file_name
-		done
-	g++ -g -Wall -o $file_name example.cpp
-	echo $DONE_TEXT
-}
-
-release () {
-	echo $INPUT_FILE_TEXT
-	read file_name
-		while [ -e "$file_name" ] ; do
-		echo $WARNING_TEXT
-		read file_name
-		done
-	g++ -O2 -Wall -o $file_name example.cpp
-	echo $DONE_TEXT
-}
-
-main () {
+print_help () {
 	clear
 	echo "Script compiles example.cpp file"
 	echo "1) Debug flag"
 	echo "2) Release flag"
 	echo "3) Exit script"
 	echo "What compilation flag you want to use:"
+}
 
+compile_file () {
+	echo $INPUT_FILE_TEXT
+	read OUTPUT_FILE_NAME
+		while [ -e "$OUTPUT_FILE_NAME" ] ; do
+		echo $WARNING_TEXT
+		read OUTPUT_FILE_NAME
+		done
+	g++ $COMPILE_FLAGS -o $OUTPUT_FILE_NAME example.cpp
+	echo $DONE_TEXT
+}
+
+main () {
+	print_help
 	read x
 
 	if [ "$x" == "1" ] ; then
-		debug
+		# 1 - Debug version
+		COMPILE_FLAGS="-g -Wall" 
+		compile_file
 
 	elif [ "$x" == "2" ] ; then
-		release
+		# 2 - Release version
+		COMPILE_FLAGS="-O2 -Wall"
+		compile_file
 
 	elif [ "$x" == "3" ]; then
+		# 3 - Exit script
 		echo "Exit, file not compiled."
 		exit
 	else 
