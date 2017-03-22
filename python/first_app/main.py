6c6474651af9c66f9cf2ccca90e8121ea3e44844
Kurossa <4mariusz4@wp.pl>
2017-03-22 20:52:25 +0100

    Update main.py, correct script to be run as application.

--- python/first_app/main.py
+++ python/first_app/main.py
@@ -1,1 +1,11 @@
-print ('Hello world')
+#!/usr/bin/python3
+import sys
+
+
+def print_function(args_num=0):
+    print('Hello World! with',args_num,'arg(s).')
+
+
+# This allows the file to be used as a SCRIPT
+if __name__ == "__main__":
+    print_function(len(sys.argv))
