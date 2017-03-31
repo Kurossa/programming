b6473e54c159a370cd1a2091db718e4307339419
Kurossa <4mariusz4@wp.pl>
2017-03-31 23:04:06 +0200

    Correct whitespaces, remove blank lines on main.py in first_app

--- python/first_app/main.py
+++ python/first_app/main.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-
-
 def print_function(args_num=0):
 
     print('Hello World! with',args_num,'arg(s).')
     for i in range(args_num):
         print (sys.argv[i],'\n')
 
-
-
 # This allows the file to be used as a SCRIPT
 if __name__ == "__main__":
-
-
-
-
-
     print_function(len(sys.argv))
+
