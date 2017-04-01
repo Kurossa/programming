9e80bece88963055cc541ec364a6bf370d723cf8
Kurossa <4mariusz4@wp.pl>
2017-04-01 22:25:53 +0200

    Fix Button class, add simple animation

--- python/calculator_app/calculator_gui.py
+++ python/calculator_app/calculator_gui.py
@@ -10,6 +10,7 @@ HEIGHT = 50
 TIMEOUT = 100
 ALLOWED_CHARACTERS = '0123456789+-*/='
 
+
 def filter_char(char):
     for c in ALLOWED_CHARACTERS:
         if c == char:
@@ -23,6 +24,7 @@ def print_function(args_num=0):
 
     for i in range(args_num):
         print(sys.argv[i],'\n')
+
 def init_curses():
     curses.initscr()
     curses.beep()
@@ -48,23 +50,38 @@ def test(stdscr):
     # Get resulting contents
     message = box.gather()
 
+class MainWindow(object):
+    def __init__(self, width, height, title):
+        self.width = width
+        self.height = height
 
 class Button(object):
-    def __init__(self, key, window, pos_x, pos_y):
-        self.WIDTH = 4
+    def __init__(self, key, pos_x, pos_y):
+        self.WIDTH = 7
         self.HEIGHT = 3
         self.key = key
-        self.window = window
+        #self.window = window
         self.pos_x = pos_x * self.WIDTH
         self.pos_y = pos_y * self.HEIGHT
+        self.text_x = int(self.WIDTH/2)
+        self.text_y = int(self.HEIGHT/2)
+        self.window = curses.newwin(self.HEIGHT, self.WIDTH, self.pos_y, self.pos_x)
+        self.render('default')
 
-    def render(self):
-        #rectangle(self.window, self.pos_y, self.pos_x, self.pos_y + self.HEIGHT, self.pos_x + self.WIDTH)
-        self.window.addstr(self.pos_x+1, self.pos_y+1, self.key)
+    def render(self, color):
+        self.window.clear()
+        self.window.border(0)
+        if (color == 'red'):
+            self.window.addstr(self.text_y, self.text_x, str(self.key), curses.color_pair(1))
+        else:
+            self.window.addstr(self.text_y, self.text_x, str(self.key))
         self.window.refresh()
 
     def animate(self):
-        self.window.refersh()
+        self.render('red')
+        sleep(0.1)
+        self.render('default')
+
 
     #@property
     #def get_key(self):
@@ -79,13 +96,15 @@ if __name__ == "__main__":
     window.timeout(TIMEOUT)
     window.keypad(1)
     window.border(0)
+    window.refresh()
 
-    key_1 = Button(window, '1', 0, 0)
-    #key_2 = Button(window, '2', 0, 1)
+    key_1 = Button('1', 3, 3)
+    key_2 = Button('2', 3, 4)
 
-    #key_1.render()
-    #key_2.render()
+    key_1.animate()
+    key_2.animate()
 
+    #sleep(3)
     #test(window)
     event_stored = 1
 
@@ -97,27 +116,36 @@ if __name__ == "__main__":
 
 
     try:
-
+        #sleep(1)
         while True:
-            window.clear()
-            window.border(0)
-            window.addstr(0, 5, 'Calculator v0.1', curses.color_pair(1))
-            window.addstr(1, 1, 'Result:'+result_str)
-            window.addstr(5,1, 'Result from engine: '+calculator_engine.insert_char('d') )
+             window.clear()
+             window.border(0)
+             window.addstr(0, 5, 'Calculator v0.1', curses.color_pair(1))
+             window.addstr(1, 1, 'Result:'+result_str)
+             #window.addstr(5,1, 'Result from engine: '+calculator_engine.insert_char('d') )
 
-            if event_stored > 0 and event_stored < 255:
-                key = filter_char(chr(event_stored))
-                #print(key)
-                window.addstr(3, 1, key, curses.color_pair(1))
-            window.addstr(4, 1, str(event_stored))
+             if event_stored > 0 and event_stored < 255:
+                 key = filter_char(chr(event_stored))
+                 if key == '1':
+                     key_1.animate()
+                 elif key == '2':
+                     key_2.animate()
+                 #print(key)
+                 window.addstr(3, 1, key, curses.color_pair(1))
+             window.addstr(4, 1, str(event_stored))
+             window.refresh()
+             key_1.render('default')
+             key_2.render('default')
 
-            event = window.getch()
-            if event > 0:
-                event_stored = event
-
-            if event == 27:
-                """ESC button meaning, Exit app."""
-                break
+             #sleep(3)
+             #break
+             event = window.getch()
+             if event > 0:
+                 event_stored = event
+        #
+             if event == 27:
+                 """ESC button meaning, Exit app."""
+                 break
     finally:
         #print_function(len(sys.argv))
         # #sleep(3)
