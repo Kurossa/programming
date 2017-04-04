477c5b83f33f543b83c6d1f6b868a235066578a1
GlabLukasz <glab_lukasz@o2.pl>
2017-04-04 23:33:34 +0200

    Add proccess_char and calculate functions.

--- python/calculator_app/calculator_engine.py
+++ python/calculator_app/calculator_engine.py
@@ -2,34 +2,99 @@
 import sys
 import math
 
-number_1 = 5
-number_2 = 5
-math_result = ''
+buffer = ''
+operation = ''
+number_1 = 0.0
+number_2 = 0.0
+math_result = 0.0
+# Allowed characters for numbers
+# 0123456789.
+# Allowed functions
+# +-*/=
 
-def insert_char(char):
-    if char == 'd':
-        return
-    print(str(char))
-    if char == '+':
-        print('Dzialanie dodawania')
-    #math_result += 'Test'
-    number_1 = 5
+def filter_char(char):
+    global number_1
+    return char
+    # if char == 1234567890.-+*/=
+    #   return char
+    # else
+    #   return ''
+
+
+def proccess_char(char):
+    global buffer
+    global number_1
+    global number_2
+    global operation
+    global math_result
+
+    allowed_chars_for_number = '1234567890.'
+    is_char_allowed_for_number = False
+
+    for allowed_char in allowed_chars_for_number:
+        if allowed_char == char:
+            is_char_allowed_for_number = True
+            break
+
+    if is_char_allowed_for_number:
+        buffer += char      # 43575
+    else:                   #only for other signs +-*/=
+        if operation == '':
+            number_1 = float(buffer)
+            buffer = ''
+            operation = char
+        else:
+            number_2 = float(buffer)
+            #print(number_1)
+            #print(number_2)
+            math_result = claculate(number_1, number_2, operation)
+            #print(math_result)
+            operation = char
+            buffer = ''
+            number_1 = math_result
+
+
+def claculate(num_1, num_2, op):
+    # can be +-*/=
+    #print('Result of ' + op + ' :')
+    if op == '+':
+        return num_1 + num_2
+    elif op == '-':
+        return num_1 - num_2
+    elif op == '*':
+        return num_1 * num_2
+    elif op == '/':
+        return num_1 / num_2
+
+
+
+
 
 def get_result_str():
-    print(str(number_1))
+    print(str(math_result))
     return math_result
 
 
-
-
 def main():
-    test_str = '100.3+10='
+    test_str2 = '2343sdfg+4dfg3575-sdfg22+123sdfg*2/sdfg1sdfg2'
+    test_str3 = ''
+    for single_char in test_str2:
+        test_str3 += filter_char(single_char)
+
+    print(test_str2)
+    print(test_str3)
+
+
+    test_str = '2343+43575-22+123*2/12'
+    print('Is same as?')
+    print(test_str)
+
     for single_char in test_str:
-        insert_char(single_char)
+        proccess_char(single_char)
 
     get_result_str()
+
 
 # This allows the file to be used as a SCRIPT
 if __name__ == "__main__":
     main()
-
