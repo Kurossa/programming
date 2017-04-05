15580bb7f3b21e0ca1d0349057afb4800cd7a435
Kurossa <4mariusz4@wp.pl>
2017-04-05 22:06:52 +0200

    Add 3 simple tasks for learning

---
+++ python/learning/task_1.py
@@ -0,0 +1,5 @@
+Create proper python file with main function.
+
+Creat function that will create variables of all known types with any value you want. i.e. int, float etc. Print those values and print those variables types using python functions.
+
+USE ANY HELP YOU WANT (INTERNET, BOOKS) BUT PLEASE DO NOT COPY CODE, BUT REWRITE IT BY YOUR SELF LETER BY LETER.
---
+++ python/learning/task_2.py
@@ -0,0 +1,19 @@
+Create proper python file with main function.
+
+Create function that will ask you severeal question, of name, age, etc. Put users answer to variables. Put together answer as a one string an than print it in one command. 
+
+Example.
+#Ask for dogs name?
+variable = dogs_name
+#Ask for dog age?
+variable2 = dogs_age
+
+# this is only example try to do it, like in example, but also try to use formating it looks like 'Your name is: {0} and age of {1}'.Format(dogs_name,dogs_age) or any other way you like.
+reply = 'Your dogs name is: ' + dogs_name + ' and its: ' + dogs age + 'years old'
+print reply
+
+Do it for few examples, questions does not matters. You can play with diffrent build-in python functions for strings. Pleas see documentation for strings in Python:
+https://docs.python.org/3/library/string.html
+
+
+USE ANY HELP YOU WANT (INTERNET, BOOKS) BUT PLEASE DO NOT COPY CODE, BUT REWRITE IT BY YOUR SELF LETER BY LETER
