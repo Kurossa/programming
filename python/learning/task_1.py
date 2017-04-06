51ad0a325cb6b3d2cf8a25dd6da7ce0d0a8a73a5
GlabLukasz <glab_lukasz@o2.pl>
2017-04-06 14:09:41 +0200

    Why this is not printing such a sipmle thing?

--- python/learning/task_1.py
+++ python/learning/task_1.py
@@ -1,5 +1,15 @@
-Create proper python file with main function.
+#!/usr/bin/python3
+# first task for python practising purposes
 
-Creat function that will create variables of all known types with any value you want. i.e. int, float etc. Print those values and print those variables types using python functions.
+#Create proper python file with main function.
 
-USE ANY HELP YOU WANT (INTERNET, BOOKS) BUT PLEASE DO NOT COPY CODE, BUT REWRITE IT BY YOUR SELF LETER BY LETER.
+#Creat function that will create variables of all known types with any value you want. i.e. int, float etc. Print those values and print those variables types using python functions.
+
+#USE ANY HELP YOU WANT (INTERNET, BOOKS) BUT PLEASE DO NOT COPY CODE, BUT REWRITE IT BY YOUR SELF LETER BY LETER.
+
+def main():
+    char = 3
+    print (type(char))
+
+if __name__ == "main":
+    main()
