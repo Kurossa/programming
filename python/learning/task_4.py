a212935e6fa7d8eb998affa3c98cb85429f2c8b0
Kurossa <4mariusz4@wp.pl>
2017-04-25 22:45:05 +0200

    Add new task 4.

---
+++ python/learning/task_4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#Create proper python file with main function.
+
+# Create array (list) of 20 items with random numbers from 0 to 100. Than make function taht will get list and return sorted one. In python for sure there is some list function that will sort it. 
+# But to practice more find algorithm how to do it and try to implement it. 
+
+#USE ANY HELP YOU WANT (INTERNET, BOOKS) BUT PLEASE DO NOT COPY CODE, BUT REWRITE IT BY YOUR SELF LETER BY LETER.
