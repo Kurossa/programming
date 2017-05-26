7c08d10ea2a792d372f6238ae2eef50eb5e6bc16
GlabLukasz <glab_lukasz@o2.pl>
2017-05-26 19:36:13 +0200

    sort list engine prebuild

--- python/learning/task_4.py
+++ python/learning/task_4.py
@@ -8,8 +8,18 @@
 
 import random
 
+
 def sort_list():
-    
+    numbers_list = [25, 13, 5, 0, 4, 4, 22]
+    sorted_list = []
+    while numbers_list:
+        minimum = numbers_list[0]
+        for i in numbers_list:
+            if i <= minimum:
+                minimum = i
+        sorted_list.append(minimum)
+        numbers_list.remove(minimum)
+    print(sorted_list)
 
 
 def main():
@@ -22,3 +32,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    sort_list()
