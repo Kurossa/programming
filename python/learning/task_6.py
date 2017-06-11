b5ec3a4f9950d31d433be3f8ce749488c7d71443
GlabLukasz <glab_lukasz@o2.pl>
2017-06-11 20:22:54 +0200

    work in progress

--- python/learning/task_6.py
+++ python/learning/task_6.py
@@ -1,4 +1,4 @@
-import os
+#!/usr/bin/python3
 
 #CpuUsage
 #self.cpu_usage
@@ -10,18 +10,27 @@ import os
 #private function that will extract cpu usage
 # def _name(self)
 
-def main():
+#def main():
     #data = os.system("mpstat")
     #print(data)
     #cpu_usage = CpuUsage
     #while(10 times)
-      data = cpu_usage.GetCpuUsage()
+      #data = cpu_usage.GetCpuUsage()
       #print (data)
       #cpu_usage.PrintCpuUsageBar(data)
 
+import os
+import time
 
 
-
+def main():
+    data = os.popen("mpstat").read()
+    cpuusage = data[183:189]
+    times = 10
+    while times > 0:
+        print("CPU Usage = " + cpuusage)
+        times-=1
+        time.sleep(2)
 
 if __name__ == "__main__":
     main()
