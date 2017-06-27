3ee3e4e267dedcaac2211a66146947df9f72b1d6
GlabLukasz <glab_lukasz@o2.pl>
2017-06-27 20:12:57 +0200

    work in progress4

--- python/learning/task_6.py
+++ python/learning/task_6.py
@@ -19,30 +19,40 @@
       #print (data)
       #cpu_usage.PrintCpuUsageBar(data)
 
+#!/usr/bin/python3
+
 import os
 import time
 
-
 class CpuUsage:
-    def __init__(self, time):
+
+
+    def __init__(self,time):
         self.time = time
         self.cpu_data = []
+
 
     def cpu_usage_return(self):
         data = os.popen('mpstat').read()
         self.cpu_data = data[183:189]
         return self.cpu_data
 
+
     def cpu_usage_print(self):
-        while self.time > 0:
-            self.time -= 1
-            return self.cpu_data
+        while self.time>0:
+            self.time-=1
+            return cpu_data.cpu_usage_return
 
 
 def main():
     usage = CpuUsage(10)
-    usaged = usage.cpu_usage_print()
+    usaged = usage.cpu_usage_return()
     print(usaged)
+
+    while usage.time>0:
+        print(usaged)
+        usage.time-=1
+        time.sleep(2)
 
 
 if __name__ == "__main__":
