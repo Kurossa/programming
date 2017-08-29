e0e9e60c89e3622e7f44c076a3b29d66aaa73f0a
GlabLukasz <glab_lukasz@o2.pl>
2017-08-29 11:30:44 +0200

    Main task working, improvements in progress: cheking is_zipfile(y/n), checking if folder already exist, rewrite(y/n)

--- python/learning/zip_task.py
+++ python/learning/zip_task.py
@@ -6,6 +6,9 @@ def print_help():
     print('Help:')
     print('zip_task.py -z <zip file> -f <zip decompression folder>')
 
+def print_error():
+    print('Error')
+
 def parse_args(argv):
     try:
         opts, args = getopt.getopt(argv, 'hz:f:', ['help', 'zipfile=', 'folder='])
@@ -14,10 +17,10 @@ def parse_args(argv):
         sys.exit(2)
 
     is_zipfile = False
-    is_folder = False
+    is_zipfolder = False
 
     zip_file = ''
-    folder = ''
+    zip_folder = ''
 
     for opt, arg in opts:
         if opt == '-h':
@@ -27,10 +30,10 @@ def parse_args(argv):
             is_zipfile = True
             zip_file = arg
         elif opt in ('-f', '--folder'):
-            is_folder = True
-            folder = arg
+            is_zipfolder = True
+            zip_folder = arg
 
-        return zip_file, folder, (is_zipfile and is_folder)
+    return zip_file, zip_folder, (is_zipfile and is_zipfolder)
 
 def extraction(zip_file, extract_folder):
     file = zip_file
@@ -40,18 +43,17 @@ def extraction(zip_file, extract_folder):
         z.extract(name, folder)
         print('The file named:',file,', has been extracted to:',folder, 'folder')
 
-def main():
-    zip_file = "example.zip"
-    folder = "example"
-
-    extraction(zip_file, folder)
-
-    # zip_file, folder, parse_ok = parse_args(argv)
-    # if parse_ok:
-    #     extraction(zip_file, folder)
-    # else:
-    #     print_help()
-    #     sys.exit(2)
+def main(argv):
+    zip_file, zip_folder, parse_ok = parse_args(argv)
+    print(sys.argv[1:])
+    print(zip_file)
+    print(zip_folder)
+    print(parse_ok)
+    if (parse_ok):
+        extraction(zip_file, zip_folder)
+    else:
+        print_help()
+        sys.exit(2)
 
     # zf = zipfile.ZipFile
     # zf.open('example.zip', mode = 'r', pwd = None)
@@ -71,4 +73,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
