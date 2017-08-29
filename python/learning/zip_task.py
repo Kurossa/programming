3888841dbd8a136d93957989d73613e90a7194ca
GlabLukasz <glab_lukasz@o2.pl>
2017-08-29 10:12:46 +0200

    extraction added, working on parse

--- python/learning/zip_task.py
+++ python/learning/zip_task.py
@@ -4,7 +4,7 @@ import sys, getopt, zipfile
 
 def print_help():
     print('Help:')
-    print_help('zip_task.py -z <zip file> -f <zip decompression folder>')
+    print('zip_task.py -z <zip file> -f <zip decompression folder>')
 
 def parse_args(argv):
     try:
@@ -16,7 +16,7 @@ def parse_args(argv):
     is_zipfile = False
     is_folder = False
 
-    zipfile = ''
+    zip_file = ''
     folder = ''
 
     for opt, arg in opts:
@@ -25,25 +25,48 @@ def parse_args(argv):
             sys.exit()
         elif opt in ('-z', '--zipfile'):
             is_zipfile = True
-            zipfile = arg
+            zip_file = arg
         elif opt in ('-f', '--folder'):
             is_folder = True
             folder = arg
 
-        return zipfile, folder, (is_zipfile and is_folder)
+        return zip_file, folder, (is_zipfile and is_folder)
+
+def extraction(zip_file, extract_folder):
+    file = zip_file
+    folder = extract_folder
+    z = zipfile.ZipFile(file, 'r')
+    for name in z.namelist():
+        z.extract(name, folder)
+        print('The file named:',file,', has been extracted to:',folder, 'folder')
 
 def main():
-    #zipfile, folder, parse_ok = parse_args(argv)
+    zip_file = "example.zip"
+    folder = "example"
+
+    extraction(zip_file, folder)
+
+    # zip_file, folder, parse_ok = parse_args(argv)
+    # if parse_ok:
+    #     extraction(zip_file, folder)
+    # else:
+    #     print_help()
+    #     sys.exit(2)
 
     # zf = zipfile.ZipFile
     # zf.open('example.zip', mode = 'r', pwd = None)
     # zf.extractall(path = None, members = None, pwd = None)
     # zf.close()
 
-    with zipfile.ZipFile('example.zip', 'r') as zf:
-        data = zf.namelist()
-        print(data)
-
+    # with zipfile.ZipFile('example.zip', 'r') as zf:
+    #     data = zf.namelist()
+    #     print(data)
+    #
+    # #file_open = open('example.zip', 'r')
+    # z = zipfile.ZipFile('example.zip', 'r')
+    # for name in z.namelist():
+    #     folder = 'example'
+    #     z.extract(name, folder)
 
 
 
