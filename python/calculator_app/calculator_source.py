b7dc015f19a63478ddde5a6a701d4c73193431a7
GlabLukasz <glab_lukasz@o2.pl>
2017-10-18 14:45:11 +0200

    Few updates. No time to explain :)

--- python/calculator_app/calculator_source.py
+++ python/calculator_app/calculator_source.py
@@ -13,37 +13,36 @@ def keyboard_filter(str_list):
 
     return buffer
 
+def main_operations(number_2, number_1, operation):
+    # number_2 = float(number_2)
+    # number_1 = float(number_1)
+
+    if operation == '+':
+        return number_2 + number_1
+    elif operation == '-':
+        return number_2 - number_1
+    elif operation == '*':
+        return number_2 * number_1
+    elif operation == '/':
+        return number_2 / number_1
+
 def chars_process(str_list):
     alow_numbers = '1234567890.'
     alow_operations = '+-*/='
     operation = ''
-    number_2 =''
+    number_2 = ''
     number_1 = ''
-
 
     for char in str_list:
         if char in alow_numbers:
             number_2 += char
         if char in alow_operations:
+            operation = char
+
             number_1 = number_2
             number_2 = ''
-            operation = char
-            #break
 
-    return number_1, number_2, operation
-
-def main_operations(number_2, number_1, operation):
-    number_2 = float(number_2)
-    number_1 = float(number_1)
-
-    if operation == '+':
-        number_2 + number_1
-    elif operation == '-':
-        number_2 - number_1
-    elif operation == '*':
-        number_2 * number_1
-    elif operation == '/':
-        number_2 / number_1
+    return number_2, number_1, operation
 
 def main():
     test_str = "et3e;tg5gerghez43t"
@@ -51,7 +50,7 @@ def main():
     output = keyboard_filter(test_str)
     print(output)
 
-    test_str2 = 'dw3.4+sr5nyt3*gds8bv'
+    test_str2 = 'dw34+sr5nyt.3*gds8bv'
     print(keyboard_filter(test_str2))
 
     output2 = chars_process(keyboard_filter(test_str2))
