9b57ab6382572f94d9d17e20007f3835ce05d0cd
GlabLukasz <glab_lukasz@o2.pl>
2017-10-27 13:00:53 +0200

    Still solving '=' issues

--- python/calculator_app/calculator_source.py
+++ python/calculator_app/calculator_source.py
@@ -13,7 +13,7 @@ def keyboard_filter(str_list):
 
     return buffer
 
-def main_operations(number_2, number_1, operation):
+def calculation(number_2, number_1, operation):
     number_2 = float(number_2)
     number_1 = float(number_1)
 
@@ -25,35 +25,43 @@ def main_operations(number_2, number_1, operation):
         return str(number_1 * number_2)
     elif operation == '/':
         return str(number_1 / number_2)
-    elif operation == '=':
-        return str(number_1)
+
+def clearing_process(number_1, number_2, operation, char):
+    number_1 = number_2
+    operation = char
+    number_2 = ''
+
 
 def chars_process(str_list):
     alow_numbers = '1234567890.'
     alow_operations = '+-*/='
-    operation_1 = ''
-    operation_2 = ''
+    operation = ''
     number_2 = ''
     number_1 = ''
+    result = ''
 
     for char in str_list:
         if char in alow_numbers:
             number_2 += char
         if char in alow_operations:
-            operation_1 = char
-            if number_1 == '' and operation_2 == '':
+            if number_1 == '' and operation == '':
                 number_1 = number_2
-                operation_2 = operation_1
+                operation = char
                 number_2 = ''
-                operation_1 = ''
             else:
-                number_2 = main_operations(number_2, number_1, operation_2)
-                number_1 = number_2
-                operation_2 = operation_1
-                number_2 = ''
-                operation_1 = ''
+                if char == '=':
+                    result = calculation(number_2, number_1, operation)
+                    number_1 = result
+                else:
+                    result = calculation(number_2, number_1, operation)
+                    number_1 = result
+                    #number_1 = number_2
+                    operation = char
+                    number_2 = ''
 
-    return number_2, number_1,
+
+
+    return result
 
 def main():
     test_str = "et3e;tg5gerghez43t"
@@ -61,7 +69,7 @@ def main():
     output = keyboard_filter(test_str)
     print(output)
 
-    test_str2 = 'dw34+sr5nyt.3*gds8bv-yf5k=;kh90uo+89+979'
+    test_str2 = 'dw34+sr5nyt.3*gds8bv-yf5k====;+98-8'
     print(keyboard_filter(test_str2))
 
     output2 = chars_process(keyboard_filter(test_str2))
