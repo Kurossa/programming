71294423af39415a8f3c721b8cf445a2e8965ed3
GlabLukasz <glab_lukasz@o2.pl>
2017-11-20 14:15:45 +0100

    Comments added, clear button added

--- python/calculator_app/calculator_source.py
+++ python/calculator_app/calculator_source.py
@@ -3,7 +3,7 @@
 import sys, math
 
 def filter_char(input_char):
-    allowed_chars = '1234567890.+-*/='
+    allowed_chars = '1234567890.+-*/=c'
     if(type(input_char) == str):
         if(len(input_char)==1):
             if input_char in allowed_chars:
@@ -32,7 +32,7 @@ def calculation(number_2, number_1, operation):
     else:
         return 'Error operation'
 
-def multi_dot_filter(number_2, char):
+def multi_dot_filter(number_2, char): #filter preventing from appear more than one decimal point in number (only first decimal point is correct)
     if number_2 == '':
         number_2 += char
     else:
@@ -40,7 +40,6 @@ def multi_dot_filter(number_2, char):
             number_2 += ''
         else:
             number_2 += char
-
     return number_2
 
 class CalculatorEngine:
@@ -52,64 +51,47 @@ class CalculatorEngine:
         self.number_2 = ''
         self.number_1 = ''
         self.result = ''
+        self.clear = 'c'
 
     def chars_process(self, str_list):
         for char in str_list:
-            if char in self.alow_numbers:
+            if char == self.clear:  #clearing process if 'c' pressed
+                self.operation = ''
+                self.number_2 = ''
+                self.number_1 = ''
+                self.result = ''
+            if char in self.alow_numbers: #creating a number
                 self.number_2 = multi_dot_filter(self.number_2, char)
             if char in self.alow_operations:
                 if self.number_2 == '':
                     self.operation = char
                 else:
-                    if char == '=':
-                        # if self.operation == '=':
-                        #     self.number_2 = ''
-                        if self.number_1 and self.number_2 != '':# and self.operation != '=':
+                    if char == '=': #main calculation
+                        if self.number_1 and self.number_2 != '':
                             self.result = calculation(self.number_2, self.number_1, self.operation)
                             self.number_1 = self.result
                         else:
                             self.number_2 = ''
-                    else:
-                        if self.operation == '=':
-                            #self.operation = char
-                            self.number_2 = ''
+                    else: #calculation is done even if +-*/ operations are entered
+                        if self.number_1 != '':
+                            self.result = calculation(self.number_2, self.number_1, self.operation)
+                            self.number_1 = self.result
                         else:
-                            if self.number_1 != '':
-                                self.result = calculation(self.number_2, self.number_1, self.operation)
-                                self.number_1 = self.result
-
-                            else:
-                                self.number_1 = self.number_2
+                            self.number_1 = self.number_2
                         self.operation = char
-
-                    #self.operation = char
                     self.number_2 = ''
-
         return self.result
 
 def main():
-    #test_str = "et3e;tg5gerghez43t"
-
-    #output = keyboard_filter(test_str)
-    #print(output)
-
-    #test_str2 = 'dw34+sr5nyt.3*gds8bv-yf5k====;+98-8'
-    #test_str2 = '123+123='
-    #print(keyboard_filter(test_str2))
 
     # str_1 = '1123..8786.123++234=234===234=234+234='
     # calc = CalculatorEngine()
 
-    str_1 = '54*15-3/*-8=+51==='
+    str_1 = '54*15-3/*-8=+51=2+3='
     calc = CalculatorEngine()
 
     output2 = calc.chars_process(keyboard_filter(str_1))
     print(output2)
-
-
-    #
-    # calculation = main_operations(second_number, first_number, op)
-    # print(calculation)
 
 if __name__ == "__main__":
     main()
