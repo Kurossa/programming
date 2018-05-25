3ec8f44469693a182fbb7e348c5e447fbf75f442
GlabLukasz <glab_lukasz@o2.pl>
2018-05-25 14:39:39 +0200

    Zip function added

--- python/work/dvc_task.py
+++ python/work/dvc_task.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
+import zipfile
+
 
 def extracting(zip_file, extract_folder):
-
-
+    z = zip_file.ZipFile(zip_file, 'r')
+    for name in z.namelist():
+        z.extract(name, extract_folder)
+        print('The file named:', zip_file, ', has been extracted to:', extract_folder, 'folder')
 
 
 
