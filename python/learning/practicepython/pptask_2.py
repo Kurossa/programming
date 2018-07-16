61fbfc6acf0301be36586c47e4472164c70ff494
GlabLukasz <glab_lukasz@o2.pl>
2018-07-16 14:39:31 +0200

    Second simple task to practice

--- python/learning/practicepython/pptask_2.py
+++ python/learning/practicepython/pptask_2.py
@@ -7,15 +7,21 @@
 #     If the number is a multiple of 4, print out a different message.
 #     Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
 
+import math
 
 def main():
     user_number = input("Please, enter number: ")
-    user_number = int(user_number)/2
-    if user_number is int:
-        print("The number you've passed is even")
+    user_number = int(user_number)
+    # print(user_number)
+    # user_number = int(user_number)
+    # print(user_number)
+    if (user_number/4).is_integer():
+        print("The number you've passed is even and multiple of 4.")
+    elif (user_number/2).is_integer():
+        print("The number you've passed is even.")
     else:
-        print("The number you've passed is odd")
+        print("The number you've passed is odd.")
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
