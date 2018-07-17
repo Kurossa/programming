7f839d9666c692aad99cdfd13f104aa4490426dd
GlabLukasz <glab_lukasz@o2.pl>
2018-07-17 13:37:01 +0200

    Task 3 small change with integer on number variable

--- python/learning/practicepython/pptask_3.py
+++ python/learning/practicepython/pptask_3.py
@@ -18,9 +18,9 @@
 def main():
     list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     list_to_add = []
-    number = input("Enter a number. It will show only elements below that number: ")
+    number = int(input("Enter a number. It will show only elements below that number: "))
     for i in list:
-        if i < int(number):
+        if i < number:
             list_to_add.append(i)
 
     print(list_to_add)
